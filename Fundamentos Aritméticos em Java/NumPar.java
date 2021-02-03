396ad552d7a111d8d40758c9138bc855dd270d1e
thiago520 <thiago520@gmail.com>
2021-02-03 00:47:42 -0300

    add desafios em java.

---
+++ Fundamentos Aritméticos em Java/NumPar.java
@@ -0,0 +1,38 @@
+/*
+* Desafios
+* Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+* Entrada
+* Você receberá 1 valor inteiro N, onde N > 0.
+
+* Saída
+* Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
+
+* Exemplo de Entrada	Exemplo de Saída
+*       6                       2
+*                               4
+*                               6
+* */
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumPar {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+
+        for (int i = 1 ; i <= num ; i++) {
+            if ( i % 2 == 0 ) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
