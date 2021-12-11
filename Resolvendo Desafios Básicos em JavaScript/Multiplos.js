61c5697b8b1d6fd1f0db267b307518f3b3fe456d
Thiago Bernardi Parascandalo <thiago520@gmail.com>
2021-12-11 19:12:48 -0300

    add desafios javascript

---
+++ Resolvendo Desafios Básicos em JavaScript/Multiplos.js
@@ -0,0 +1,29 @@
+let lines = gets().split("\n");
+
+let line = lines.shift().split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+
+if ( A % B == 0 || B % A == 0 ) 
+    print("Sao Multiplos");                   //complete com a sua lógica
+else 
+    print("Nao sao Multiplos");
+
+/* 
+    Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+6 24
+
+Sao Multiplos
+
+6 25
+
+Nao sao Multiplos */
